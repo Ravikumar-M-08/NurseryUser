e2bd2c804bc499e7b20a9652001c0bdda803c39e
Ravikumar <ravikumar936318@gmail.com>
2025-07-05 12:16:45 +0530

    ffjft

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -6,56 +6,56 @@ const Products = () => {
         <>
             <div className='container-fluid'>
                 <div className="gardening-head">
-                    <img src="public/image/home/banner1.jpg" alt="img" />
+                    <img src="/image/home/banner1.jpg" alt="img" />
                     <h3 className="title">Products</h3>
                 </div>
                 <div>
                     <h1 className='text-center'>Our Products</h1>
                     <div className="product_section">
                         <div className="products">
-                            <img src="public/image/products/products1.jpg" alt="" />
+                            <img src="/image/products/products1.jpg" alt="" />
                             <div className="goto">
                                 <p>Gardening</p>
                                 <Link to='/gardening'>Gardening</Link>
                             </div>
                         </div>
                         <div className="products">
-                            <img src="public/image/products/products2.jpg" alt="" />
+                            <img src="/image/products/products2.jpg" alt="" />
                             <div className="goto">
                                 <p>Plants</p>
                                 <Link to='/plants'>Explore</Link>
                             </div>
                         </div>
                         <div className="products">
-                            <img src="public/image/products/products3.jpg" alt="" />
+                            <img src="/image/products/products3.jpg" alt="" />
                             <div className="goto">
                                 <p>Seeds</p>
                                 <Link to='/seeds'>Explore</Link>
                             </div>
                         </div>
                         <div className="products">
-                            <img src="public/image/products/products4.jpg" alt="" />
+                            <img src="/image/products/products4.jpg" alt="" />
                             <div className="goto">
                                 <p>Bubls</p>
                                 <Link to='/bubls'>Explore</Link>
                             </div>
                         </div>
                         <div className="products">
-                            <img src="public/image/products/products5.jpg" alt="" />
+                            <img src="/image/products/products5.jpg" alt="" />
                             <div className="goto">
                                 <p>Pots</p>
                                 <Link to='/plots'>Explore</Link>
                             </div>
                         </div>
                         <div className="products">
-                            <img src="public/image/products/products6.jpg" alt="" />
+                            <img src="/image/products/products6.jpg" alt="" />
                             <div className="goto">
                                 <p>Fertilizer</p>
                                 <Link to='/fertilizer'>Explore</Link>
                             </div>
                         </div>
                         <div className="products">
-                            <img src="public/image/products/products7.jpg" alt="" />
+                            <img src="/image/products/products7.jpg" alt="" />
                             <div className="goto">
                                 <p>Accessories</p>
                                 <Link to='/accessories'>Explore</Link>
