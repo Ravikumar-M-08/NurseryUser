ed5f6e7ac79a733ed5572900b0011b3bdc4724d8
Ravikumar <ravikumar936318@gmail.com>
2025-07-05 12:09:47 +0530

    fhdh

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
 		<>
 			<nav className="navbar navbar-expand-md bg-body-tertiary p-0">
 				<div className="container-fluid nav">
-					<Link className="navbar-brand" to="/"><img src="public/image/logo.png" alt="" width="70px" /></Link>
+					<Link className="navbar-brand" to="/"><img src="logo.png" alt="" width="70px" /></Link>
 					<label for="menu" className="menu navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
 						<input type="checkbox" name="menu" id="menu" />
 						<span></span>
@@ -25,21 +25,6 @@ function Nav() {
 								<Link to="/products" className={`nav-link ${location.pathname === '/products' ? 'active' : ''}`} >Products</Link>
 							</li>
 
-							{/* <li className="nav-item dropdown">
-								<Link className="nav-link dropdown-toggle" id='navbardrop' role="button"
-									data-bs-toggle="dropdown">
-									Product
-								</Link>
-								<ul className="dropdown-menu" aria-labelledby='navbardrop'>
-									<li><Link className="dropdown-item" to='/gardening'>Gardening</Link></li>
-									<li><Link className="dropdown-item" to='/plants'>Plants</Link></li>
-									<li><Link className="dropdown-item" to='/seeds'>Seeds</Link></li>
-									<li><Link className="dropdown-item" to='/bubls'>Bubls</Link></li>
-									<li><Link className="dropdown-item" to='/plots'>Pots</Link></li>
-									<li><Link className="dropdown-item" to='/fertilizer'>Fertilizer</Link></li>
-									<li><Link className="dropdown-item" to='/accessories'>Accessories</Link></li>
-								</ul>
-							</li> */}
 							<li className="nav-item">
 								<Link to="/about" className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}>About</Link>
 							</li>
